bc31903ace0821514d5b21d21d7bffa2cfd33c6f
Jan Strube <jan.strube@gmail.com>
2016-10-01 20:56:40 -0700

    basic filling seems to work

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,16 @@
 using Histograms
 using Base.Test
 
-x = randn(10000)
+NENTRIES = 10000
+x = randn(NENTRIES)
 h1 = H1D(100, 0, 10)
 for i in x
     hfill!(h1, i)
 end
+@test sum(h1.entries) == NENTRIES
 
 h2 = H2D(100, 0, 10, 100, 0, 10)
-y = randn(10000)
-for i = 1:10000
+y = randn(NENTRIES)
+for i = 1:NENTRIES
     hfill!(h2, x[i], y[i])
 end
