2066b53c38e74aaa147431d1d0a1c72d933060ed
Jan Strube <jan.strube@gmail.com>
2016-10-01 20:59:14 -0700

    added some basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,13 @@ using Base.Test
 
 NENTRIES = 10000
 x = randn(NENTRIES)
-h1 = H1D(100, 0, 10)
+h1 = H1D(100, -10, 10)
 for i in x
     hfill!(h1, i)
 end
 @test sum(h1.entries) == NENTRIES
 
-h2 = H2D(100, 0, 10, 100, 0, 10)
+h2 = H2D(100, -10, 10, 100, -10, 10)
 y = randn(NENTRIES)
 for i = 1:NENTRIES
     hfill!(h2, x[i], y[i])
